76597003bd68d1e17d043e0498a1194640dd4dbf
Deepth1-253 <deepthibondalapati253@gmail.com>
2025-01-31 22:25:09 +0530

    Add files via upload

---
+++ Ansible-Terraform-integration/9.vpc-peering.tf
@@ -0,0 +1,40 @@
+data "aws_vpc" "ansible_vpc" {
+  id = "vpc-066228556b9fe3c4b"
+}
+
+data "aws_route_table" "ansible_vpc_rt" {
+  subnet_id = "subnet-05b06672f71389c0c"
+  #If subnet_id giving errors use route table id as below
+  #route_table_id = data.aws_route_table.ansible_vpc_rt.id
+}
+
+resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
+  peer_vpc_id = data.aws_vpc.ansible_vpc.id
+  vpc_id      = aws_vpc.default.id
+  auto_accept = true
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = {
+    Name = "Ansible-${var.vpc_name}-Peering"
+  }
+}
+
+resource "aws_route" "peering-to-ansible-vpc" {
+  route_table_id            = aws_route_table.terraform-public.id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
+  #depends_on                = [aws_route_table.terraform-public]
+}
+
+resource "aws_route" "peering-from-ansible-vpc" {
+  route_table_id            = data.aws_route_table.ansible_vpc_rt.id
+  destination_cidr_block    = "172.37.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
+  #depends_on                = [aws_route_table.terraform-public]
+}
