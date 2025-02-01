8c8096ad21a46fae3d3a636c958c9d373e234dd1
Deepth1-253 <deepthibondalapati253@gmail.com>
2025-02-01 11:28:53 +0530

    Update 9.vpc-peering.tf

--- Ansible-Terraform-integration/9.vpc-peering.tf
+++ Ansible-Terraform-integration/9.vpc-peering.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "ansible_vpc" {
 data "aws_route_table" "ansible_vpc_rt" {
   subnet_id = "subnet-05b06672f71389c0c"
   #If subnet_id giving errors use route table id as below
-  route_table_id = data.aws_route_table.ansible_vpc_rt.id
+  #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
 
 resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
